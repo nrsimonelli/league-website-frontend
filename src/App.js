fd2341635ad6a3b826b138a1858321dd800da6d4
Nick <nrsimonelli@gmail.com>
2021-01-18 14:17:00 -0600

    flex box home page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    hello
+    <div className='Root' >
+      <div className="Nav">
+        <div>title</div>
+        <div>
+          icon/link container
+        </div>
+      </div> 
+      <div className="Comp-1">
+      Welcome! (cover image )
+      </div> 
+      <div className="Comp-2">
+        <div className='button-cont'>
+          <div className='button-1'>button-1</div>
+          <div className='button-2'>button-2</div>
+        </div>
+      </div>   
+      <div className="Comp-6">
+        Footer text
+      </div>     
     </div>
   );
 }

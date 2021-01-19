4d8f0bbbed5a13c2b5d7e65e5aeac7809d60a461
Nicholas Simonelli <nrsimonelli@gmail.com>
2021-01-18 21:46:45 -0600

    moved Nav into its own component and began button import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,22 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import { Button } from 'antd';
 import './App.css';
+
+import Nav from '../Nav/Nav'
 
 class App extends Component {
 
   render() {
     return (
       <div className='Root' >
-        <div className="Nav">
-          <div>title</div>
-          <div>
-            icon/link container
-          </div>
-        </div> 
+        <Nav />
         <div className="Comp-1">
           <div className='Cover-Image'></div>
         </div> 
         <div className="Comp-2">
           <div className='button-cont'>
-            <div className='button-1'>button-1</div>
-            <div className='button-2'>button-2</div>
+            <Button type='primary' size='large' >Primary</Button>
+            <Button type='ghost' size='large'>Ghost</Button>
           </div>
         </div>   
         <div className="Comp-6">
@@ -31,4 +28,4 @@ class App extends Component {
   
 }
 
-export default connect()(App);
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+
+  render() {
+    return (
+        <div className="Nav">
+          <div>title</div>
+          <div>
+            icon/link container
+          </div>
+        </div> 
+    );
+  }
+  
+}
+
+export default Nav;

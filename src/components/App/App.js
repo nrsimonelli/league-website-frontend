de402c0d2c042c8bc364a24d0395f6f58c65e5ec
Nicholas Simonelli <nrsimonelli@gmail.com>
2021-01-19 16:47:25 -0600

    made footer opaque and increased cover image size. changed button spacing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,15 +11,16 @@ class App extends Component {
       <div className='Root' >
         <Nav />
         <div className="Comp-1">
-        <div className='button-cont'>
+          <div className='button-cont'>
             <Button type='primary'  size='large' >View</Button>
             <Button type='danger'  size='large'>Log in</Button>
           </div> 
+          <div className="Comp-6">
+          developed by / donate here / contribute here
+          </div> 
         </div> 
            
-        <div className="Comp-6">
-          developed by
-        </div>     
+            
       </div>
     );
   }

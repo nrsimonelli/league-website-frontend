df0b65d607c90c280cd12958cdec478e87cc1691
Nick <nrsimonelli@gmail.com>
2021-01-25 21:02:26 -0600

    used calc for responsive gutter width

--- src/components/League/League.js
+++ src/components/League/League.js
@@ -5,8 +5,10 @@ class League extends Component {
   render() {
     return (
         <div className="League-root">
-          <div>
-            Record Game
+          <div className='League-cont'>
+            <div className='League-div'>
+              League Table
+            </div>
           </div>
         </div> 
     );

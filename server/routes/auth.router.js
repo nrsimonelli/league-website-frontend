b4d2d6b0983d85d7f68ddffed594615bc5b54785
Nicholas Simonelli <nrsimonelli@gmail.com>
2021-02-05 16:47:03 -0600

    created a route in auth for sending user data to client

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -3,13 +3,15 @@ const passport = require('passport');
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
-router.get('/', passport.authenticate('discord'));
-
 router.get('/login', passport.authenticate('discord', {
     failureRedirect: '/forbidden',
     successRedirect: 'http://localhost:3000/'
-
 }));
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    res.send(req.user);
+    console.log('testing req.user', req.user);
+})
 
 router.get('/test', rejectUnauthenticated, (req, res) => {
     res.sendStatus(200);

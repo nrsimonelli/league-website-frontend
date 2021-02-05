6b925376353481457cffa337138567a13fafdd12
Nicholas Simonelli <nrsimonelli@gmail.com>
2021-02-05 16:54:34 -0600

    created user saga for passing user info to client post login

---
+++ src/redux/sagas/userSaga.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { response } from 'express';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* fetchUser() {
+  try{
+
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    }
+
+    const response = yield axios.get('/auth/', config);
+
+    yield put({ type: 'SET_USER', payload: response.data });
+
+  } catch (err) {
+    console.log('user get request failed', error);
+  }
+} 
+
+function* userSaga() {
+  yield takeLatest('FETCH_USER', fetchUser);
+}
+
+
+export default userSaga;

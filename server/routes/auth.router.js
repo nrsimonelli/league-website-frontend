6ebc27a965918f34b29d4d08ce8d6368af9c11c3
Nicholas Simonelli <nrsimonelli@gmail.com>
2021-02-04 19:42:07 -0600

    added auth middleware and changed column names in user table

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -4,21 +4,10 @@ const router = express.Router();
 
 router.get('/', passport.authenticate('discord'));
 
-router.get('/login', passport.authenticate('discord'), (req, res) => {
-    res.sendStatus(200);
-});
+router.get('/login', passport.authenticate('discord', {
+    failureRedirect: '/forbidden',
+    successRedirect: '/'
 
-router.get('/login', (req, res) => {
-    if (req.user) {
-        res.send(req.user);
-    } else {
-        res.status(401).send({msg: 'Unauthorized' });
-    }
-    
-})
-
-
-
-
+}));
 
 module.exports = router;

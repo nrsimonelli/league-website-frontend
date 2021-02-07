5a1ab2f606d6099ba2445fe48aa295c46a57cad7
Nick <nrsimonelli@gmail.com>
2021-02-06 23:20:25 -0600

    turned record from class to function component and imported basic modal

--- src/components/Record/Record.js
+++ src/components/Record/Record.js
@@ -1,20 +1,42 @@
-import React, {Component} from 'react';
-import { Button } from 'antd';
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
 
-class Record extends Component {
 
-  render() {
-    return (
-        <div className="Rec-root">
-          <div className="Rec-cont">
-            <Button>
-              Record Game
-            </Button>
-          </div>
-        
-        </div> 
-    );
-  }
+
+
+const Record = () => {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+  
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+      <div className="Rec-root">
+        <div className="Rec-cont">
+          <Button onClick={showModal}>
+            Record Game
+          </Button>
+          
+    <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      </Modal>
+        </div>
+      
+      </div> 
+  );
+  
   
 }
 

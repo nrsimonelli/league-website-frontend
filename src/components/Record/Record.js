7bb08fa7997075f66dbc8922f7713525d2c9de3c
Nick <nrsimonelli@gmail.com>
2021-02-06 18:55:37 -0600

    skeleton for record game component

--- src/components/Record/Record.js
+++ src/components/Record/Record.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import { Button } from 'antd';
 
 class Record extends Component {
 
   render() {
     return (
         <div className="Rec-root">
-          <div>
-            Record Game
+          <div className="Rec-cont">
+            <Button>
+              Record Game
+            </Button>
           </div>
+        
         </div> 
     );
   }

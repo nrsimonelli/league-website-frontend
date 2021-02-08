2e25539600a94f5233cdc725316b40f2064467b9
Nick <nrsimonelli@gmail.com>
2021-02-08 17:51:06 -0600

    basic structure for form in modal

--- src/components/Record/Record.js
+++ src/components/Record/Record.js
@@ -1,43 +1,93 @@
 import React, { useState } from 'react';
-import { Modal, Button, Select, Form, Input } from 'antd';
+import { Modal, Button, Select,  Form, Input } from 'antd';
 
-const CustomizedForm = ({ onChange, fields }) => {
+const { Option } = Select;
+
+const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
+
+  const [form] = Form.useForm();
+  
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  }
+
   return (
-    <Form
-      name="global_state"
-      layout="inline"
-      fields={fields}
-      onFieldsChange={(_, allFields) => {
-        onChange(allFields);
+    <Modal
+      visible={visible}
+      title="Active Game List"
+      okText="Create"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch((info) => {
+            console.log('Validate Failed:', info);
+          });
       }}
     >
-      <Form.Item
-        name="username"
-        label="Username"
-        rules={[
-          {
-            required: true,
-            message: 'Username is required!',
-          },
-        ]}
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
       >
-        <Input />
-      </Form.Item>
-    </Form>
+        <Form.Item name="activeGame" className="collection-create-form_last-form-item">
+          <Select placeholder='Select Game' onChange={handleChange}>
+            <Option value="game1">Game 1</Option>
+            <Option value="game2">Game 2</Option>
+            <Option value="game3">Game 3</Option>
+          </Select>
+        </Form.Item>
+        {/* <Form.Item
+          name="title"
+          label="Title"
+          rules={[
+            {
+              required: true,
+              message: 'Please input the title of collection!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item> */}
+        
+        
+      </Form>
+    </Modal>
   );
 };
-
-
+  
 
 const Record = () => {
+  const [visible, setVisible] = useState(false);
 
+  const onCreate = (values) => {
+    console.log('Received values of form: ', values);
+    setVisible(false);
+  };
 
   return (
       <div className="Rec-root">
         <div className="Rec-cont">
-          <Button>
-            Record Game
-          </Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          RecordGame
+        </Button>
+        <CollectionCreateForm
+          visible={visible}
+          onCreate={onCreate}
+          onCancel={() => {
+            setVisible(false);
+          }}
+        />
           
           
         </div>
@@ -45,6 +95,6 @@ const Record = () => {
       </div> 
   );
   
-}
+};
 
 export default Record;

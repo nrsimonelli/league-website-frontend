13f6494cc04d783b0b1bee654fed9c3c510eeb18
Nick <nrsimonelli@gmail.com>
2021-02-09 11:03:29 -0600

    created player mat and player faction selections

--- src/components/Record/Record.js
+++ src/components/Record/Record.js
@@ -3,6 +3,26 @@ import { Modal, Button, Select,  Form, Input } from 'antd';
 
 const { Option } = Select;
 
+const MATS = [
+  {label: 'Industrial', value: 'Industrial'},
+  {label: 'Engineering', value: 'Engineering'},
+  {label: 'Militant', value: 'Militant'},
+  {label: 'Patriotic', value: 'Patriotic'},
+  {label: 'Innovative', value: 'Innovative'},
+  {label: 'Mechanical', value: 'Mechanical'},
+  {label: 'Agricultural', value: 'Agricultural'},
+];
+
+const FACTIONS = [
+  {label: 'Crimea', value: 'Crimea'},
+  {label: 'Saxony', value: 'Saxony'},
+  {label: 'Polania', value: 'Polania'},
+  {label: 'Albion', value: 'Albion'},
+  {label: 'Nordic', value: 'Nordic'},
+  {label: 'Rusviet', value: 'Rusviet'},
+  {label: 'Togawa', value: 'Togawa'},
+];
+
 const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
 
   const [form] = Form.useForm();
@@ -35,12 +55,18 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
         layout="vertical"
         name="form_in_modal"
       >
-        <Form.Item name="activeGame" className="collection-create-form_last-form-item">
+        <Form.Item name="activeGameList" >
           <Select placeholder='Select Game' onChange={handleChange}>
             <Option value="game1">Game 1</Option>
             <Option value="game2">Game 2</Option>
             <Option value="game3">Game 3</Option>
           </Select>
+        </Form.Item>
+        <Form.Item name="playerMat" >
+          <Select placeholder='Player Mat' options={MATS} onChange={handleChange} />
+        </Form.Item>
+        <Form.Item name="playerFaction" >
+          <Select placeholder='Player Faction' options={FACTIONS} onChange={handleChange} />
         </Form.Item>
         {/* <Form.Item
           name="title"
